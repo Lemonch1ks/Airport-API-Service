7a4a28281c0248b97c9aff9c717f4c31d3f29041
Oleksandr Dvirko <dvalex02@gmail.com>
2026-07-20 21:17:25 +0300

    added permissions

--- airport/views.py
+++ airport/views.py
@@ -1,17 +1,15 @@
 from rest_framework import viewsets, mixins
 
+from airport.permissions import IsAdminOrReadOnly
+
 from airport.models import Airport
+
 from airport.serializers import (
     AirportSerializer,
 
 )
 
-class AirportViewSet(
-    viewsets.GenericViewSet,
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.RetrieveModelMixin,
-):
+class AirportViewSet(viewsets.ModelViewSet):
     queryset = Airport.objects.all()
     serializer_class = AirportSerializer
+    permission_classes = [IsAdminOrReadOnly,]
