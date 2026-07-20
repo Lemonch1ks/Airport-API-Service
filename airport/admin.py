3fba1de38ef78ee92018735cc1224b561bdcbdd4
Oleksandr Dvirko <dvalex02@gmail.com>
2026-07-20 20:24:15 +0300

    added airport models to admin panmel

--- airport/admin.py
+++ airport/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from airport.models import (
+    Airport,
+    Crew,
+    Flight,
+    Airplane,
+    AirplaneType,
+    Order,
+    Route,
+    Ticket,
+)
+
+admin.site.register(Airport)
+admin.site.register(Crew)
+admin.site.register(Flight)
+admin.site.register(AirplaneType)
+admin.site.register(Order)
+admin.site.register(Route)
+admin.site.register(Ticket)
+admin.site.register(Airplane)
